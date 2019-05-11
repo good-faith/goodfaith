fd0ed6e11d207568aadbd3f81e3460e3984f2ffe
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-05-10 22:45:20 -0500

    + ui lib, declare data lib

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Data } from '@goodfaith/data';
+import { UiModule } from '@goodfaith/ui';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, UiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
