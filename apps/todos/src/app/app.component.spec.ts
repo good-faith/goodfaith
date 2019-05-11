639fcb42f8ef843de6bd31349317559860ae30be
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-05-11 06:19:05 -0500

    added cypress scripts, more config

--- apps/todos/src/app/app.component.spec.ts
+++ apps/todos/src/app/app.component.spec.ts
@@ -25,7 +25,8 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to todos!'
+      'Todos!'
     );
   });
 });
+
