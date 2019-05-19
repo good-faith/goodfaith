d3a77ec792590b7c8b0c917a53a010c66a18a8a6
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-05-19 12:31:52 -0500

    updated cypress config

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,5 @@
 describe('The Home Page', function() {
     it('successfully loads', function() {
-      cy.visit('/') // change URL to match your dev URL
+      cy.visit('http://localhost:4200/') // change URL to match your dev URL
     })
   })
