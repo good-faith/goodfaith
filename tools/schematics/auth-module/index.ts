4c4ad988685f7691cefe21618338874b16355c21
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-12-18 15:16:38 -0600

    added path to auth schema

--- tools/schematics/auth-module/index.ts
+++ tools/schematics/auth-module/index.ts
@@ -1,9 +1,26 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import * as path from 'path';
+
 
 export default function(schema: any): Rule {
   return chain([
-    externalSchematic('@nrwl/schematics', 'lib', {
-      name: schema.name
+    // externalSchematic('@nrwl/schematics', 'lib', {
+    //   name: schema.name,
+    //   project: schema.project
+    // }), 
+    externalSchematic('@schematics/angular', 'module', {
+      project: schema.project,
+      name: schema.name,
+      path: path.join(
+        'apps',
+        schema.project,
+        'src',
+        'app',
+        schema.name,
+        'services'
+        ),
+      routing: true,
+      module: 'app.module.ts'
     })
-  ]);
+  ])
 }
