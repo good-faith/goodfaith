cf0fd9706d99f0808aa6c92837ba338f91a9395e
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-12-19 14:55:31 -0600

    + styled.div to ticket

--- apps/tickets/src/app/app.tsx
+++ apps/tickets/src/app/app.tsx
@@ -10,7 +10,41 @@ import { Ticket } from '@goodfaith/data'
 
 
 const StyledApp = styled.div`
-  ...
+  font-family: sans-serif;
+  min-width: 300px;
+  max-width: 600px;
+  margin: 50px auto;
+
+  .flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  header {
+    background-color: #143055;
+    color: white;
+    padding: 5px;
+    border-radius: 3px;
+  }
+
+  main {
+    padding: 0 36px;
+  }
+
+  h1 {
+    text-align: center;
+    margin-left: 18px;
+    font-size: 24px;
+  }
+  
+  .ticket {
+    color: #0094ba;
+    height: 36px;
+    background-color: rgba(0, 0, 0, 0);
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    border-radius: 4px;
+  }
 `;
 
 
