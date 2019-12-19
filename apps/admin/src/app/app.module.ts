c5a159a371a8ee9eeb5cf3e42fcd4c1c11f4f198
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-12-18 23:24:13 -0600

    update packages to ^8

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -11,25 +11,35 @@ import {
   appReducer
 } from './+state/app.reducer';
 import { AppEffects } from './+state/app.effects';
-import { NxModule } from '@nrwl/nx';
+import { NxModule } from '@nrwl/angular';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
-import { AdminDashboardModule, AdminDashboardRoutes } from '@goodfaith/admin/dashboard';
+import {
+  AdminDashboardModule,
+  AdminDashboardRoutes
+} from '@goodfaith/admin/dashboard';
 import { AuthAdminModule } from './auth-admin/auth-admin.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{path: 'admin-dashboard', children: AdminDashboardRoutes}], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(
+      [{ path: 'admin-dashboard', children: AdminDashboardRoutes }],
+      { initialNavigation: 'enabled' }
+    ),
     NxModule.forRoot(),
     StoreModule.forRoot(
       { app: appReducer },
       {
         initialState: { app: appInitialState },
-        metaReducers: !environment.production ? [] : [], runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true }
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true
+        }
       }
     ),
     EffectsModule.forRoot([AppEffects]),
