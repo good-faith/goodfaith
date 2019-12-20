72511a053ae2a258e7b9c174d0822e2df88f7988
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-12-19 18:44:42 -0600

    - my-site, my-site-api & my-site-e2e

--- apps/my-site-api/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'my-site-api',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/my-site-api'
-};
--- apps/my-site/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'my-site',
-  preset: '../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/my-site/'
-};
