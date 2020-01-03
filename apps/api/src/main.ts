cbc23abce152b8f7bd0937af476ead5c8e6883d4
Jason Sprouse <jason.a.sprouse@gmail.com>
2020-01-03 04:17:48 -0600

    enabled cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors();
+  app.enableCors();
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
