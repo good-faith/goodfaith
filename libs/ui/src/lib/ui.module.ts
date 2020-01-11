cc38410ff23442afac364b2e52371824ab6f7a83
Jason Sprouse <jason.a.sprouse@gmail.com>
2020-01-11 12:43:00 -0600

    - Home and Profile component in uiModule

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
+
+
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent, ProfileComponent],
-  exports: [HomeComponent, ProfileComponent]
+  declarations: [],
+  exports: []
 })
 export class UiModule {}
